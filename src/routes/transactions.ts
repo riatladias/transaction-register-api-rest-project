481e9bc1485d98d3e8697e473c1a136816c024ad
Riatla Dias <riatla.d@gmail.com>
2023-02-23 14:57:24 -0300

    adc readme

---
+++ src/routes/transactions.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const body = createTransactionBodySchema.parse(request.body)
+
+    return body
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import fastify from 'fastify'
-import { knex } from './database'
+
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
