8df32ab138f5ed33ab3958f9a7b3a44791440757
Riatla Dias <riatla.d@gmail.com>
2023-02-24 15:33:12 -0300

    adc tipos da tabela no knex

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,17 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { randomUUID } from 'crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (request) => {
+  app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     })
 
-    const body = createTransactionBodySchema.parse(request.body)
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
-    return body
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
