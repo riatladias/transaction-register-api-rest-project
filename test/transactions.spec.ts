31c70a3ea59c593ccaa349293722b8f0725d7d5d
Riatla Dias <riatla.d@gmail.com>
2023-03-02 00:05:58 -0300

    adc banco de teste

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,7 +12,12 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
-  it.only('should be able to create a new transaction', async () => {
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -22,5 +28,27 @@ describe('Transactions routes', () => {
       .expect(201)
   })
 
-  it('should be able to list all transactions', async () => {})
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
